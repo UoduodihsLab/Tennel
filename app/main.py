2348f13aebffb1634acae3576d12db4f580db7bd
Uoduodihs <uoduodihs@gmail.com>
2025-08-26 21:10:40 +0800

    项目启动方式改为使用uvicorn命令行, 不直接从脚本运行

--- app/main.py
+++ app/main.py
@@ -54,5 +54,5 @@ register_tortoise(
     add_exception_handlers=True,
 )
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=True)
